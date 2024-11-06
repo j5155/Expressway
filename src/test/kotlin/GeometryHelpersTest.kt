03a5983c5ee2b3354fd84bb02d0f906668d9cae2
j5155 <54331556+j5155@users.noreply.github.com>
2024-11-05 19:25:52 -0900

    Test GeometryHelpers

---
+++ src/test/kotlin/GeometryHelpersTest.kt
@@ -0,0 +1,34 @@
+import com.acmerobotics.roadrunner.Pose2d
+import com.acmerobotics.roadrunner.Vector2d
+import page.j5155.actionAdditions.distanceTo
+import page.j5155.actionAdditions.rotateBy
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class GeometryHelpersTest {
+    @Test
+    fun vectorDistanceTo() {
+        val vector1 = Vector2d(0.0, 0.0)
+        val vector2 = Vector2d(5.0, 0.0)
+        assertEquals(vector1.distanceTo(vector2), 5.0)
+    }
+    @Test
+    fun vectorRotateBy() { // relies on rotateBy, times, and distanceTo passing
+        val startVector = Vector2d(5.0,0.0)
+        val rotationAmount = Math.toRadians(90.0)
+
+        val rotatedVector = startVector.rotateBy(rotationAmount)
+        val targetVector = Vector2d(0.0,5.0)
+        assert(rotatedVector.distanceTo(targetVector) < 1e-15) // math imprecision
+    }
+    @Test
+    fun poseRotateBy() {
+        val startPose = Pose2d(5.0,0.0,0.0)
+        val rotationAmount = Math.toRadians(90.0)
+
+        val rotatedPose = startPose.rotateBy(rotationAmount)
+        val targetPose = Pose2d(0.0,5.0,Math.toRadians(90.0))
+        assert(rotatedPose.distanceTo(targetPose) < 1e-15 && rotatedPose.heading - targetPose.heading < 1e-15)
+    }
+
+}
