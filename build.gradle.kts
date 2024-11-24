ca9d96526391d0b0fe56ab582150003367d81a24
j5155 <54331556+j5155@users.noreply.github.com>
2024-11-24 12:33:54 -0900

    add ftc module (TODO configure publishing)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,16 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    id("com.android.library") version "8.7.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
+
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
+    google()
 }
+
 
 kotlin {
     jvmToolchain(18)
