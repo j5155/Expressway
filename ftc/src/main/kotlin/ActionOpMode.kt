6856714c37f9db0bf7aee6f0a4a94aa180bceab7
j5155 <54331556+j5155@users.noreply.github.com>
2024-11-24 17:56:26 -0900

    add action runner to opmodes

--- ftc/src/main/kotlin/ActionOpMode.kt
+++ ftc/src/main/kotlin/ActionOpMode.kt
@@ -1,5 +1,27 @@
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket
+import com.acmerobotics.roadrunner.Action
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 
-abstract class ActionOpMode : OpMode() {
-    protected val actionRunner = ActionRunner()
+abstract class ActionOpMode(val runner: ActionRunner = ActionRunner()) : OpMode() {
+    /**
+     * Run an action asynchronously.
+     * Note: you MUST run updateActions every loop so that the actions queued by runAsync continue.
+     */
+    protected fun runAsync(action: Action) {
+        runner.runAsync(action)
+    }
+
+
+    protected fun updateActions(packet: TelemetryPacket? = null) {
+        if (packet == null) {
+            runner.updateAsync()
+        } else {
+            runner.updateAsync(packet)
+        }
+    }
+
+
+    @Deprecated("legacy wrapper",ReplaceWith("runAsync(action)"))
+    protected fun run(action: Action) = runAsync(action)
 }
