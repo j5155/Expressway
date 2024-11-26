ab685b27d4d5cd455e5309c0c7cfa03d79dd8418
j5155 <54331556+j5155@users.noreply.github.com>
2024-11-25 15:46:23 -0900

    add comment

--- ftc/src/main/kotlin/ActionRunner.kt
+++ ftc/src/main/kotlin/ActionRunner.kt
@@ -1,5 +1,7 @@
 import com.acmerobotics.dashboard.FtcDashboard
 
+
+// should this exist? should this wrapping happen only in OpModes?
 /**
  * ActionRunner allows for running actions asynchronously during an OpMode.
  * This just wraps NoPreviewActionRunner from core and uses FtcDashboard for preview.
