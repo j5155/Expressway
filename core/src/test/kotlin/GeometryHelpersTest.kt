e51aaf5c1cfa8fe81007cbbc7f095149a18d9a75
j5155 <54331556+j5155@users.noreply.github.com>
2024-11-25 18:51:41 -0900

    use approxequal

--- core/src/test/kotlin/GeometryHelpersTest.kt
+++ core/src/test/kotlin/GeometryHelpersTest.kt
@@ -1,5 +1,6 @@
 import com.acmerobotics.roadrunner.Pose2d
 import com.acmerobotics.roadrunner.Vector2d
+import page.j5155.expressway.geometry.approxEqual
 import page.j5155.expressway.geometry.distanceTo
 import page.j5155.expressway.geometry.rotateBy
 import kotlin.test.Test
@@ -13,13 +14,20 @@ class GeometryHelpersTest {
         assertEquals(vector1.distanceTo(vector2), 5.0)
     }
     @Test
+    fun vectorApproxEqual() {
+        val vector1 = Vector2d(0.0, 0.0)
+        val vector2 = Vector2d(1e-20, 0.0)
+        assert(vector1 != vector2)
+        assert(vector1.approxEqual(vector2))
+    }
+    @Test
     fun vectorRotateBy() { // relies on rotateBy, times, and distanceTo passing
         val startVector = Vector2d(5.0,0.0)
         val rotationAmount = Math.toRadians(90.0)
 
         val rotatedVector = startVector.rotateBy(rotationAmount)
         val targetVector = Vector2d(0.0,5.0)
-        assert(rotatedVector.distanceTo(targetVector) < 1e-15) // math imprecision
+        assert(rotatedVector.approxEqual(targetVector))
     }
     @Test
     fun poseDistanceTo() {
@@ -29,13 +37,20 @@ class GeometryHelpersTest {
         assertEquals(pose1.distanceTo(pose2), 5.0)
     }
     @Test
+    fun poseApproxEqual() {
+        val pose1 = Pose2d(0.0,0.0,0.0)
+        val pose2 = Pose2d(1e-20,0.0,1e-20)
+        assert(pose1 != pose2)
+        assert(pose1.approxEqual(pose2))
+    }
+    @Test
     fun poseRotateBy() {
         val startPose = Pose2d(5.0,0.0,0.0)
         val rotationAmount = Math.toRadians(90.0)
 
         val rotatedPose = startPose.rotateBy(rotationAmount)
         val targetPose = Pose2d(0.0,5.0,Math.toRadians(90.0))
-        assert(rotatedPose.distanceTo(targetPose) < 1e-15 && rotatedPose.heading - targetPose.heading < 1e-15)
+        assert(rotatedPose.approxEqual(targetPose))
     }
 
 }
