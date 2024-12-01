a058499517a4284dd943348062a731caab7bf0e5
j5155 <54331556+j5155@users.noreply.github.com>
2024-12-01 13:26:51 -0900

    remove string from gamepad

--- ftc/src/main/kotlin/page/j5155/expressway/ftc/GamepadEx.kt
+++ ftc/src/main/kotlin/page/j5155/expressway/ftc/GamepadEx.kt
@@ -10,24 +10,24 @@ import kotlin.math.abs
  * @param gamepad the Qualcomm gamepad to observe. It should be `gamepad1` or `gamepad2`, unless you're doing something funky.
  */
 class GamepadEx(gamepad: Gamepad) {
-    val a = Button("A") { gamepad.a }
-    val b = Button("B") { gamepad.b }
-    val x = Button("X") { gamepad.x }
-    val y = Button("Y") { gamepad.y }
+    val a = Button { gamepad.a }
+    val b = Button { gamepad.b }
+    val x = Button { gamepad.x }
+    val y = Button { gamepad.y }
 
-    val dpadUp = Button("DPad Up") { gamepad.dpad_up }
-    val dpadDown = Button("DPad Down") { gamepad.dpad_down }
-    val dpadLeft = Button("DPad Left") { gamepad.dpad_left }
-    val dpadRight = Button("DPad Right") { gamepad.dpad_right }
+    val dpadUp = Button { gamepad.dpad_up }
+    val dpadDown = Button { gamepad.dpad_down }
+    val dpadLeft = Button { gamepad.dpad_left }
+    val dpadRight = Button { gamepad.dpad_right }
 
-    val leftBumper = Button("Left Bumper") { gamepad.left_bumper }
-    val rightBumper = Button("Right Bumper") { gamepad.right_bumper }
+    val leftBumper = Button { gamepad.left_bumper }
+    val rightBumper = Button { gamepad.right_bumper }
 
-    val leftTrigger = Trigger("Left Trigger") { gamepad.left_trigger }
-    val rightTrigger = Trigger("Right Trigger") { gamepad.right_trigger }
+    val leftTrigger = Trigger { gamepad.left_trigger }
+    val rightTrigger = Trigger { gamepad.right_trigger }
 
-    val leftStick = JoyStick("Left Stick", { gamepad.left_stick_x }, { gamepad.left_stick_y }, Button("Left Stick Button") { gamepad.left_stick_button })
-    val rightStick = JoyStick("Right Stick", { gamepad.right_stick_x }, { gamepad.right_stick_y }, Button("Right Stick Button") { gamepad.right_stick_button })
+    val leftStick = JoyStick ({ gamepad.left_stick_x }, { gamepad.left_stick_y }, Button { gamepad.left_stick_button })
+    val rightStick = JoyStick ({ gamepad.right_stick_x }, { gamepad.right_stick_y }, Button { gamepad.right_stick_button })
 
     val controls = listOf(a, b, x, y, dpadUp, dpadDown, dpadLeft, dpadRight, leftBumper, rightBumper,
         leftTrigger, rightTrigger, leftStick, rightStick)
@@ -58,7 +58,7 @@ class GamepadEx(gamepad: Gamepad) {
      * @param controlToWatch a lambda for the control to watch. This will be re-invoked every time update is called, so
      *          that it will automatically match the gamepad's value
      */
-    inner class Button(private val name: String = "Unknown Button", private val controlToWatch: () -> Boolean):
+    inner class Button(private val controlToWatch: () -> Boolean):
         Control {
         /**
          * Whether this button is currently pushed down
@@ -116,14 +116,6 @@ class GamepadEx(gamepad: Gamepad) {
 
             return actionList
         }
-
-        override fun toString(): String {
-            val set = mutableSetOf<String>()
-            if (down) set.add("Down")
-            if (justPressed) set.add("Just Pressed")
-            if (justReleased) set.add("Just Released")
-            return if (set.isNotEmpty()) "$name: ${set.joinToString(", ")}" else ""
-        }
     }
 
     /**
@@ -132,7 +124,7 @@ class GamepadEx(gamepad: Gamepad) {
      * @param controlToWatch a lambda for the control to watch. This will be re-invoked every time update is called, so
      *          that it will automatically match the gamepad's value
      */
-    inner class Trigger(private val name: String = "Unknown Trigger", private val controlToWatch: () -> Float):
+    inner class Trigger(private val controlToWatch: () -> Float):
         Control {
         /**
          * How far off of default the trigger should move before it is considered "down"
@@ -199,20 +191,6 @@ class GamepadEx(gamepad: Gamepad) {
 
             return actionList
         }
-
-        /**
-         * Returns a string showing whether the trigger was just pressed, was just released, and/or
-         * how much it's being pressed down.
-         * @return whether the trigger was just pressed, was just released, and/or how much it's
-         * being pressed down.
-         */
-        override fun toString(): String {
-            val set = mutableSetOf<String>()
-            if (justPressed) set.add("Just Pressed")
-            if (justReleased) set.add("Just Released")
-            if (amount != 0.0f) set.add("Amount Pressed: $amount")
-            return if (set.isNotEmpty()) "$name: ${set.joinToString(", ")}" else ""
-        }
     }
 
     /**
@@ -222,7 +200,7 @@ class GamepadEx(gamepad: Gamepad) {
      * @param yToWatch the y value to watch
      * @param button the button to watch
      */
-    inner class JoyStick(private val name: String, private val xToWatch: () -> Float, private val yToWatch: () -> Float, private val button: Button):
+    inner class JoyStick(private val xToWatch: () -> Float, private val yToWatch: () -> Float, private val button: Button):
         Control {
         /**
          * How far off center the joystick should move before it is considered "down"
@@ -297,26 +275,6 @@ class GamepadEx(gamepad: Gamepad) {
             }
 
             return actionList
-        }
-
-        /**
-         * Returns a string showing whether the button was just pressed, was just released, and/or
-         * how much it's being pressed down, as well as whether the joystick just started or
-         * stopped moving, and its x and y coordinates (if they aren't 0).
-         * @return information about the joystick's current state
-         */
-        override fun toString(): String {
-            val set = mutableSetOf<String>()
-            if (button.down) set.add("Button Down")
-            if (button.justPressed) set.add("Button Just Pressed")
-            if (button.justReleased) set.add("Button Just Released")
-            if (justMoved) set.add("Just Moved Off-Center")
-            if (justCentered) set.add("Just Moved Back to Center")
-            if (set.isNotEmpty() || x != 0.0f || y != 0.0f) {
-                set.add("X: $x")
-                set.add("Y: $y")
-            }
-            return if (set.isNotEmpty()) "$name: ${set.joinToString(", ")}" else ""
         }
     }
 
