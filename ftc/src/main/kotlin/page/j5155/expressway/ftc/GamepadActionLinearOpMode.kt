ae51db8e459a9c6a07b792b6aa43ce593f34ec0f
Rowan McAlpin <87948250+rowan-mcalpin@users.noreply.github.com>
2024-11-30 21:52:17 -0800

    Docs & default parameter

--- ftc/src/main/kotlin/page/j5155/expressway/ftc/GamepadActionLinearOpMode.kt
+++ ftc/src/main/kotlin/page/j5155/expressway/ftc/GamepadActionLinearOpMode.kt
@@ -1,3 +1,8 @@
 package page.j5155.expressway.ftc
 
-abstract class GamepadActionLinearOpMode(override val runner: GamepadActionRunner): ActionLinearOpMode()
+/**
+ * This is an OpMode wrapper that lets you use a GamepadActionRunner (and its associated features) without dispersing
+ * casts throughout your code.
+ * @param runner a GamepadActionRunner (you don't need to override this, it handles that automatically)
+ */
+abstract class GamepadActionLinearOpMode(override val runner: GamepadActionRunner = GamepadActionRunner()): ActionLinearOpMode()
