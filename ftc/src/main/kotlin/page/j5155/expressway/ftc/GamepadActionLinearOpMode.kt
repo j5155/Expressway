ba3a771b4fff4ca8514674e2e26fb0f46940058f
j5155 <54331556+j5155@users.noreply.github.com>
2024-12-01 14:08:54 -0900

    improve GamepadActionRunner to remove initialize and vars and lateinit

--- ftc/src/main/kotlin/page/j5155/expressway/ftc/GamepadActionLinearOpMode.kt
+++ ftc/src/main/kotlin/page/j5155/expressway/ftc/GamepadActionLinearOpMode.kt
@@ -3,6 +3,7 @@ package page.j5155.expressway.ftc
 /**
  * This is an OpMode wrapper that lets you use a GamepadActionRunner (and its associated features) without dispersing
  * casts throughout your code.
- * @param runner a GamepadActionRunner (you don't need to override this, it handles that automatically)
  */
-abstract class GamepadActionLinearOpMode(override val runner: GamepadActionRunner = GamepadActionRunner()): ActionLinearOpMode()
+abstract class GamepadActionLinearOpMode(): ActionLinearOpMode() {
+    override val runner: GamepadActionRunner = GamepadActionRunner(gamepad1, gamepad2)
+}
--- ftc/src/main/kotlin/page/j5155/expressway/ftc/GamepadActionRunner.kt
+++ ftc/src/main/kotlin/page/j5155/expressway/ftc/GamepadActionRunner.kt
@@ -5,25 +5,16 @@ import com.qualcomm.robotcore.hardware.Gamepad
 /**
  * This is an ActionRunner that runs actions automatically when certain buttons are pressed on the gamepads.
  */
-class GamepadActionRunner: UniqueActionRunner() {
+class GamepadActionRunner(gamepad1: Gamepad, gamepad2: Gamepad): UniqueActionRunner() {
     /**
      * A GamepadEx reference used for mapping actions to buttons/joysticks/triggers
      */
-    lateinit var gamepad1: GamepadEx
+    val gamepad1 = GamepadEx(gamepad1)
 
     /**
      * A GamepadEx reference used for mapping actions to buttons/joysticks/triggers
      */
-    lateinit var gamepad2: GamepadEx
-
-    /**
-     * This function initializes the gamepads. It MUST be called prior to the first time update() is called. Ideally is
-     * called as the first thing in the OpMode.
-     */
-    fun initialize(gamepad1: Gamepad, gamepad2: Gamepad) {
-        this.gamepad1 = GamepadEx(gamepad1)
-        this.gamepad2 = GamepadEx(gamepad2)
-    }
+    val gamepad2 = GamepadEx(gamepad2)
 
     /**
      * This function should be called every loop, as it handles updating the gamepads and running their mapped actions
