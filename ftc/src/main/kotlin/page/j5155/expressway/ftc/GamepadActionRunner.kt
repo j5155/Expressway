bb356f2c50b3a9715f5b75b1d5fa77be190b1825
Rowan McAlpin <87948250+rowan-mcalpin@users.noreply.github.com>
2024-11-30 21:21:27 -0800

    Fix bug, add initialize() function

--- ftc/src/main/kotlin/page/j5155/expressway/ftc/GamepadActionRunner.kt
+++ ftc/src/main/kotlin/page/j5155/expressway/ftc/GamepadActionRunner.kt
@@ -5,16 +5,25 @@ import com.qualcomm.robotcore.hardware.Gamepad
 /**
  * This is an ActionRunner that runs actions automatically when certain buttons are pressed on the gamepads.
  */
-class GamepadActionRunner(qualcommGamepad1: Gamepad, qualcommGamepad2: Gamepad): UniqueActionRunner() {
+class GamepadActionRunner: UniqueActionRunner() {
     /**
-     * A page.j5155.expressway.ftc.GamepadEx reference used for mapping actions to buttons/joysticks/triggers
+     * A GamepadEx reference used for mapping actions to buttons/joysticks/triggers
      */
-    val gamepad1 = GamepadEx(qualcommGamepad1)
+    lateinit var gamepad1: GamepadEx
 
     /**
-     * A page.j5155.expressway.ftc.GamepadEx reference used for mapping actions to buttons/joysticks/triggers
+     * A GamepadEx reference used for mapping actions to buttons/joysticks/triggers
      */
-    val gamepad2 = GamepadEx(qualcommGamepad2)
+    lateinit var gamepad2: GamepadEx
+
+    /**
+     * This function initializes the gamepads. It MUST be called prior to the first time update() is called. Ideally is
+     * called as the first thing in the OpMode.
+     */
+    fun initialize(gamepad1: Gamepad, gamepad2: Gamepad) {
+        this.gamepad1 = GamepadEx(gamepad1)
+        this.gamepad2 = GamepadEx(gamepad2)
+    }
 
     /**
      * This function should be called every loop, as it handles updating the gamepads and running their mapped actions
