ea8e0e858d1bcdf74e64bb80f2187013fc909b99
j5155 <54331556+j5155@users.noreply.github.com>
2024-12-04 16:15:15 -0900

    fix SquidController

--- ftc/src/main/kotlin/page/j5155/expressway/ftc/motion/SquidController.kt
+++ ftc/src/main/kotlin/page/j5155/expressway/ftc/motion/SquidController.kt
@@ -35,7 +35,7 @@ class SquidController: PIDFController {
         measuredPosition: Double,
         measuredVelocity: Double?
     ): Double {
-        val result = update(timestamp, measuredPosition, measuredVelocity)
+        val result = super.update(timestamp, measuredPosition, measuredVelocity)
 
         return sqrt(abs(result)) * sign(result)
     }
