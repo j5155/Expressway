1ce8931e1343a5e1537a5b385d53f82b85b84b11
j5155 <54331556+j5155@users.noreply.github.com>
2024-12-15 17:33:28 -0900

    make ILC work properly

--- core/src/main/kotlin/page/j5155/expressway/core/actions/InitLoopCondAction.kt
+++ core/src/main/kotlin/page/j5155/expressway/core/actions/InitLoopCondAction.kt
@@ -4,14 +4,13 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket
 import com.acmerobotics.roadrunner.Action
 import java.util.function.Supplier
 
-typealias Condition = Supplier<Boolean>
 
 /**
  * Allows for the creation of an Action using init and loop methods and a boolean-returning continuity condition.
  * Like InitLoopAction, but with the continuity condition seperated out
  * @param condition continues the action while the condition is true; false to stop
  */
-abstract class InitLoopCondAction protected constructor(val condition: Condition) : Action {
+abstract class InitLoopCondAction protected constructor(val condition: Supplier<Boolean>) : Action {
     /**
      * Initializes the action.
      * This will always run before [loop].
@@ -34,7 +33,7 @@ abstract class InitLoopCondAction protected constructor(val condition: Condition
     var stop = false
 
     val isRunning: Boolean
-        get() = condition()
+        get() = condition.get()
 
     final override fun run(p: TelemetryPacket): Boolean {
         if (!hasInit) {
@@ -44,14 +43,12 @@ abstract class InitLoopCondAction protected constructor(val condition: Condition
 
         loop(p)
 
-        return if (condition()) {
+        return if (condition.get()) {
             true
         } else {
             cleanup()
             false
         }
     }
-
-    private operator fun Condition.invoke() = this.get()
 }
 
